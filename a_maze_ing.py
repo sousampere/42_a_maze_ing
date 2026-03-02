e0d194cb67b0e6d51f1247676f7a905485e9b751
Laurent Bonnet <laurent.bonnet26@gmail.com>
2026-03-02 11:35:32 +0100

    update: flake8 in a_maze_ing.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -23,7 +23,9 @@ if __name__ == "__main__":
         print(f'{Colors.RED}{e}{Colors.END}', file=sys.stderr)
         exit(1)
     except ValidationError as e:
-        print(f'{Colors.RED}Configuration error for {e.errors()[0]['loc'][0]}: {e.errors()[0]['msg']}, got {e.errors()[0]['input']}.{Colors.END}', file=sys.stderr)
+        print(f'{Colors.RED}Configuration error for {e.errors()[0]['loc'][0]}:'
+              f' {e.errors()[0]['msg']}, got {e.errors()[0]['input']}.'
+              f'{Colors.END}', file=sys.stderr)
         exit(1)
     maze = Maze(config)
     debug(maze)
@@ -32,19 +34,18 @@ if __name__ == "__main__":
 
     # maze.visualize()
     if len(maze.get_protected_cells()) == 0:
-        print(f'{Colors.RED}Your configuration made displaying the 42 pattern impossible.{Colors.END}', file=sys.stderr)
+        print(f'{Colors.RED}Your configuration made displaying '
+              f'the 42 pattern impossible.{Colors.END}', file=sys.stderr)
     try:
         maze.output_maze('output_maze.txt')
     except PermissionError:
-        print(f'{Colors.RED}Could not open your output file: no permissions.{Colors.END}', file=sys.stderr)
+        print(f'{Colors.RED}Could not open your output file: '
+              f'no permissions.{Colors.END}', file=sys.stderr)
 
     print(maze.config)
     maze.make_maze_perfect(maze.config.perfect)
     maze.visualize()
-    print('v')
     path_finder = PathFinder()
-    print('p')
     print(PathFinder.find_path(maze))
-    print('m')
     main()
     tcflush(sys.stdin.fileno(), TCIFLUSH)
