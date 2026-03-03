d2106ab1fe67d49c308c639aea62e9fb4d810246
Laurent Bonnet <laurent.bonnet26@gmail.com>
2026-03-03 15:13:16 +0100

    update: flake8 on MazeVisualizer.py

--- src/objects/MazeVisualizer.py
+++ src/objects/MazeVisualizer.py
@@ -28,8 +28,10 @@ class MazeVisualizer():
                 elif (x == maze.config.exit_coords['x']
                       and y == maze.config.exit_coords['y']):
                     center_char = '🚀'
-                elif (maze.config.show_path and maze.is_path_cell(x, y)['status']):
-                    center_char = f'\033[1;37m{maze.is_path_cell(x, y)['emoji']}{maze.control.color}'
+                elif (maze.config.show_path
+                      and maze.is_path_cell(x, y)['status']):
+                    center_char = f'\033[1;37m\
+                    {maze.is_path_cell(x, y)['emoji']}{maze.control.color}'
                 else:
                     center_char = '  '
                 match char.get_hex_value():
