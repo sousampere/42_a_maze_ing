b6d4a60dd9dbc2cd7e59a719b24535c5db41eb90
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-03-03 10:35:23 +0100

    update: made a_maze_ing.py better

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -11,12 +11,14 @@ from termios import TCIFLUSH, tcflush
 
 
 def main() -> None:
-    print("Hello from a-maze-ing!")
+    # Getting prompt arguments
+    try:
+        args = get_args()
+    except Exception:
+        print(f'{Colors.RED}Could not get your arguments.'
+              f' Please use a valid argument.{Colors.END}', file=sys.stderr)
 
-
-if __name__ == "__main__":
-    args = get_args()
-
+    # Config object setup
     try:
         config = get_parsed_config(args['config'])
     except (ConfigError, ParsingError) as e:
@@ -27,32 +29,43 @@ if __name__ == "__main__":
               f' {e.errors()[0]['msg']}, got {e.errors()[0]['input']}.'
               f'{Colors.END}', file=sys.stderr)
         exit(1)
+
+    # Maze preparation
     maze = Maze(config)
     maze_generator = MazeGenerator(True)
     sys.stdout.write("\033[?25l")
-    maze_generator.generate(maze)
-    Controller().stop_listener()
-    sys.stdout.write("\033[?25h")
 
-    # maze.visualize()
+    # Maze generation
+    maze_generator.generate(maze)
+    Controller().stop_listener()  # Stop the keys listening
+    sys.stdout.write("\033[?25h")  # Place the cursor at the start of the terminal
+
+    # Apply holes in the walls to make it unperfect
+    maze.make_maze_perfect(maze.config.perfect)
+    MazeVisualizer.visualize(maze)  # Visualization with holes
+
+    # Path finding
+    PathFinder.find_path(maze)
+    tcflush(sys.stdin.fileno(), TCIFLUSH)
+
+    # Output the maze to the output file
+    try:
+        maze.output_maze(config.output_file)
+    except PermissionError:
+        print(f'{Colors.RED}Could not open your output file: '
+              f'no permissions.{Colors.END}', file=sys.stderr)
+    except Exception:
+        print(f'{Colors.RED}Could not write your output file: '
+              f'Please use a valid one.{Colors.END}', file=sys.stderr)
+
+    # Display an error message if displaying the 42 logo is impossible
     if len(maze.get_protected_cells()) == 0:
         print(f'{Colors.RED}Your configuration made displaying '
               f'the 42 pattern impossible.{Colors.END}', file=sys.stderr)
-    try:
-        maze.output_maze('output_maze.txt')
-    except PermissionError:
-        print(f'{Colors.RED}Could not open your output file: '
-              f'no permissions.{Colors.END}', file=sys.stderr)
 
-    print(maze.config)
-    maze.make_maze_perfect(maze.config.perfect)
-    MazeVisualizer.visualize(maze)
-    path_finder = PathFinder()
-    print(PathFinder.find_path(maze))
-    main()
-    tcflush(sys.stdin.fileno(), TCIFLUSH)
+if __name__ == "__main__":
     try:
-        maze.output_maze('output_maze.txt')
-    except PermissionError:
-        print(f'{Colors.RED}Could not open your output file: '
-              f'no permissions.{Colors.END}', file=sys.stderr)
+        main()
+    except Exception as e:
+        print(f'{Colors.RED}Something went wrong with the maze: '
+              f'{e}{Colors.END}', file=sys.stderr)
