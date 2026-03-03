d64721373fed90280014bfe4b55ed9a937d0bb2a
Laurent Bonnet <laurent.bonnet26@gmail.com>
2026-03-03 10:16:22 +0100

    add: except for KeyboardInterrupt in a_maze_ing.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -16,43 +16,46 @@ def main() -> None:
 
 if __name__ == "__main__":
     args = get_args()
-
     try:
-        config = get_parsed_config(args['config'])
-    except (ConfigError, ParsingError) as e:
-        print(f'{Colors.RED}{e}{Colors.END}', file=sys.stderr)
-        exit(1)
-    except ValidationError as e:
-        print(f'{Colors.RED}Configuration error for {e.errors()[0]['loc'][0]}:'
-              f' {e.errors()[0]['msg']}, got {e.errors()[0]['input']}.'
-              f'{Colors.END}', file=sys.stderr)
-        exit(1)
-    maze = Maze(config)
-    maze_generator = MazeGenerator(True)
-    sys.stdout.write("\033[?25l")
-    maze_generator.generate(maze)
-    Controller().stop_listener()
-    sys.stdout.write("\033[?25h")
+        try:
+            config = get_parsed_config(args['config'])
+        except (ConfigError, ParsingError) as e:
+            print(f'{Colors.RED}{e}{Colors.END}', file=sys.stderr)
+            exit(1)
+        except ValidationError as e:
+            print(f'{Colors.RED}Configuration error for {e.errors()[0]['loc'][0]}:'
+                f' {e.errors()[0]['msg']}, got {e.errors()[0]['input']}.'
+                f'{Colors.END}', file=sys.stderr)
+            exit(1)
+        maze = Maze(config)
+        maze_generator = MazeGenerator(True)
+        sys.stdout.write("\033[?25l")
+        maze_generator.generate(maze)
+        Controller().stop_listener()
 
-    # maze.visualize()
-    if len(maze.get_protected_cells()) == 0:
-        print(f'{Colors.RED}Your configuration made displaying '
-              f'the 42 pattern impossible.{Colors.END}', file=sys.stderr)
-    try:
-        maze.output_maze('output_maze.txt')
-    except PermissionError:
-        print(f'{Colors.RED}Could not open your output file: '
-              f'no permissions.{Colors.END}', file=sys.stderr)
+        # maze.visualize()
+        if len(maze.get_protected_cells()) == 0:
+            print(f'{Colors.RED}Your configuration made displaying '
+                f'the 42 pattern impossible.{Colors.END}', file=sys.stderr)
+        try:
+            maze.output_maze('output_maze.txt')
+        except PermissionError:
+            print(f'{Colors.RED}Could not open your output file: '
+                f'no permissions.{Colors.END}', file=sys.stderr)
 
-    print(maze.config)
-    maze.make_maze_perfect(maze.config.perfect)
-    MazeVisualizer.visualize(maze)
-    path_finder = PathFinder()
-    print(PathFinder.find_path(maze))
-    main()
-    tcflush(sys.stdin.fileno(), TCIFLUSH)
-    try:
-        maze.output_maze('output_maze.txt')
-    except PermissionError:
-        print(f'{Colors.RED}Could not open your output file: '
-              f'no permissions.{Colors.END}', file=sys.stderr)
+        print(maze.config)
+        maze.make_maze_perfect(maze.config.perfect)
+        MazeVisualizer.visualize(maze)
+        path_finder = PathFinder()
+        print(PathFinder.find_path(maze))
+        main()
+        tcflush(sys.stdin.fileno(), TCIFLUSH)
+        try:
+            maze.output_maze('output_maze.txt')
+        except PermissionError:
+            print(f'{Colors.RED}Could not open your output file: '
+                  f'no permissions.{Colors.END}', file=sys.stderr)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sys.stdout.write("\033[?25h")
